0612bc2eefa656dcfda0beed7629bfd98e35ffa5
defauth98 <neto.daniribeiro@gmail.com>
2021-02-25 11:49:27 -0300

    Exercicio bonus: Multiplicando os items do array pelo proximo

---
+++ bloco_4/dia_2/ex-bonus3.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i + 1] === undefined) {
+    newNumbers.push(numbers[i] * 2);
+  } else {
+    newNumbers.push(numbers[i] * numbers[i + 1]);
+  }
+}
+
+console.log(newNumbers);
