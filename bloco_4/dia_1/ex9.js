df54fd2af7535c8b34d23a6a4cb1972245b76b9c
defauth98 <neto.daniribeiro@gmail.com>
2021-02-25 10:19:29 -0300

    Exercicio sobre if/else - Verifica os angulos formam um triangulo

---
+++ bloco_4/dia_1/ex9.js
@@ -0,0 +1,9 @@
+let anguloA = 40;
+let anguloB = 40;
+let anguloC = 100;
+
+if (anguloA + anguloB + anguloC === 180) {
+  console.log('Eh um triangulo');
+} else {
+  console.log('Não eh um triangulo');
+}
