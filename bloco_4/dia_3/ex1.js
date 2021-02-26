5bd5b4c4e70c25538cce496aff299f221e1a0bc0
defauth98 <neto.daniribeiro@gmail.com>
2021-02-26 13:01:08 -0300

    Fix: Mudando o nome da variavel string para asterisks

--- bloco_4/dia_3/ex1.js
+++ bloco_4/dia_3/ex1.js
@@ -1,11 +1,11 @@
 const n = 5;
 
 for (let index = 0; index <= n; index += 1) {
-  let string = "";
+  let asterisks = '';
 
   for (let insideIndex = 0; insideIndex <= n; insideIndex += 1) {
-    string += "*";
+    asterisks += '*';
   }
 
-  console.log(string);
+  console.log(asterisks);
 }
