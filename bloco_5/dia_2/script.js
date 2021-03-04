c0fbb4af67ba760f0715703e968b39b00e728f93
defauth98 <neto.daniribeiro@gmail.com>
2021-03-04 11:50:25 -0300

    Segunda parte dos exercicios

--- bloco_5/dia_2/script.js
+++ bloco_5/dia_2/script.js
@@ -1,18 +1,19 @@
 const body = document.body;
 const title = document.createElement('h1');
-const div1 = document.createElement('div');
-const div2 = document.createElement('div');
-const div3 = document.createElement('div');
-const div4 = document.createElement('div');
+const mainContentDiv = document.createElement('div');
+const centerContentDiv = document.createElement('div');
+const leftContentDiv = document.createElement('div');
+const rightContentDiv = document.createElement('div');
 const paragraph = document.createElement('p');
 const image = document.createElement('img');
 const list = document.createElement('ul');
 
-div1.classList.add('main-content');
-div2.classList.add('center-content');
-div3.classList.add('left-content');
-div4.classList.add('right-content');
+mainContentDiv.classList.add('main-content');
+centerContentDiv.classList.add('center-content');
+leftContentDiv.classList.add('left-content');
+rightContentDiv.classList.add('right-content');
 image.classList.add('small-image');
+title.classList.add('title');
 
 title.innerHTML = 'Exercício 5.2 - JavaScript DOM';
 paragraph.innerHTML = 'Exercicios sobre criação de elemento na dom via JS';
@@ -27,10 +28,31 @@ for (let index = 0; index < 10; index += 1) {
   list.appendChild(listItem);
 }
 
-div4.appendChild(list);
-div1.appendChild(div2);
-div1.appendChild(div3);
-div1.appendChild(div4);
-div2.appendChild(paragraph);
+for (let index = 0; index < 3; index += 1) {
+  const title = document.createElement('h1');
+
+  title.classList.add('description');
+
+  mainContentDiv.appendChild(title);
+}
+
+rightContentDiv.style.marginRight = 'auto';
+centerContentDiv.style.backgroundColor = 'green';
+
+// Referência: Gabarito da trybe
+list.lastChild.remove();
+list.lastChild.remove();
+
+rightContentDiv.appendChild(list);
+leftContentDiv.appendChild(image);
+
+mainContentDiv.appendChild(centerContentDiv);
+mainContentDiv.appendChild(leftContentDiv);
+mainContentDiv.appendChild(rightContentDiv);
+
+centerContentDiv.appendChild(paragraph);
+
+mainContentDiv.removeChild(leftContentDiv);
+
 body.appendChild(title);
-body.appendChild(div1);
+body.appendChild(mainContentDiv);
