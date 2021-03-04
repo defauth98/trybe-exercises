92261b21eaab9391dbca0f494fe76e8a3faf28b8
defauth98 <neto.daniribeiro@gmail.com>
2021-03-04 11:32:57 -0300

    Exercicio 04

--- bloco_5/dia_2/script.js
+++ bloco_5/dia_2/script.js
@@ -2,10 +2,16 @@ const body = document.body;
 const title = document.createElement('h1');
 const div1 = document.createElement('div');
 const div2 = document.createElement('div');
+const paragraph = document.createElement('p');
 
 title.innerHTML = 'Exercício 5.2 - JavaScript DOM';
 div1.classList.add('main-content');
 div2.classList.add('center-content');
 
+paragraph.innerHTML = 'Exercicios sobre criação de elemento na dom via JS';
+
+div2.appendChild(paragraph);
+
 body.appendChild(title);
 body.appendChild(div1);
+body.appendChild(div2);
