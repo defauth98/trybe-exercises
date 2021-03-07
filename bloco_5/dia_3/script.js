c5fc5328ba7a1a3679c31315898816b667ed95a2
defauth98 <neto.daniribeiro@gmail.com>
2021-03-07 11:50:56 -0300

    Exercicio 02

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -19,7 +19,7 @@ function createDaysOfTheWeek() {
   }
 }
 
-// Exercicio 01
+// Exercício 01
 function createCalendarDays() {
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
@@ -44,7 +44,7 @@ function createCalendarDays() {
   }
 }
 
-// Exercicio 02
+// Exercício 02
 function createHolidayButton(){
   const buttonElement = document.createElement('button');
   buttonElement.id = 'btn-holiday';
