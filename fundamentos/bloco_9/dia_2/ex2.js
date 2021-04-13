d694201a2a5ee55c4d9144a53b6766b8f28c3bc4
defauth98 <neto.daniribeiro@gmail.com>
2021-04-13 10:47:54 -0300

    Exercicio 03

--- fundamentos/bloco_9/dia_2/ex2.js
+++ fundamentos/bloco_9/dia_2/ex2.js
@@ -10,12 +10,18 @@ const promise = new Promise((resolve, reject) => {
     soma += (number * number);
   })
 
-  if(soma < 8000){
-    resolve();
+  if(soma < 8000){   
+    const item1 = (soma / 2).toFixed(2);
+    const item2 = (soma / 3).toFixed(2);
+    const item3 = (soma / 5).toFixed(2);
+    const item4 = (soma / 10).toFixed(2);
+    const numbersArray = [item1, item2, item3, item4];
+
+    resolve(numbersArray);
   } else {
     reject();
   }
 
 })
-  .then(() => console.log('Promise resolvida'))
+  .then((numbers) => console.log('Promise resolvida', numbers))
   .catch(() => console.log('Promise rejeitada'))
