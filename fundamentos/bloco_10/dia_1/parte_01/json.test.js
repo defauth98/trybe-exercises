f104da8b8af70525edbf104fa7eda31e92e97efc
defauth98 <neto.daniribeiro@gmail.com>
2021-04-15 16:25:04 -0300

    Parte 01: Testes para a funcao json

---
+++ fundamentos/bloco_10/dia_1/parte_01/json.test.js
@@ -0,0 +1,11 @@
+const { obj1, obj2, obj3 } = require('./json');
+
+describe('Compare dois objetos (JSON) para verificar se são idênticos ou não', () => {
+  test('Verifique se o obj1 e igual ao obj2', () => {
+    expect(obj1).toEqual(obj2);
+  });
+
+  test('Verifique se o obj1 nao e igual ao obj2', () => {
+    expect(obj1).not.toEqual(obj3);
+  });
+});
