cc1dc5956fa6f1547e1d4a68edc52f4200ff4e55
defauth98 <neto.daniribeiro@gmail.com>
2021-04-30 17:35:00 -0300

    Adicionando os estados

--- frontend/bloco_12/dia_2/my-forms-2.0/src/App.js
+++ frontend/bloco_12/dia_2/my-forms-2.0/src/App.js
@@ -1,45 +1,52 @@
+import { Component } from 'react';
+
 import FormLine from './components/FormLine';
+import ComboBox from './components/ComboBoxStates';
+
 import './styles/global.css';
 import './styles.css';
 
-function App() {
-  return (
-    <main>
-      <form action="">
-        <FormLine
-          inputName="Nome"
-          placeholder="Seu nome"
-          limit={40}
-          toUpperCase
-          required
-        />
-        <FormLine
-          inputName="Email"
-          placeholder="seu_email@mail.com"
-          limit={50}
-          required
-        />
-        <FormLine
-          inputName="CPF"
-          placeholder="0000-0000/00"
-          limit={11}
-          required
-        />
-        <FormLine
-          inputName="Endereço"
-          placeholder="Rua X - Bairo Y - Numero Z"
-          limit={200}
-          required
-        />
-        <FormLine
-          inputName="Cidade"
-          placeholder="São Paulo"
-          limit={28}
-          required
-        />
-      </form>
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <form action="">
+          <FormLine
+            inputName="Nome"
+            placeholder="Seu nome"
+            limit={40}
+            toUpperCase
+            required
+          />
+          <FormLine
+            inputName="Email"
+            placeholder="seu_email@mail.com"
+            limit={50}
+            required
+          />
+          <FormLine
+            inputName="CPF"
+            placeholder="0000-0000/00"
+            limit={11}
+            required
+          />
+          <FormLine
+            inputName="Endereço"
+            placeholder="Rua X - Bairo Y - Numero Z"
+            limit={200}
+            required
+          />
+          <FormLine
+            inputName="Cidade"
+            placeholder="São Paulo"
+            limit={28}
+            required
+          />
+          <ComboBox />
+        </form>
+      </main>
+    );
+  }
 }
 
 export default App;
--- frontend/bloco_12/dia_2/my-forms-2.0/src/components/FormLine/index.jsx
+++ frontend/bloco_12/dia_2/my-forms-2.0/src/components/FormLine/index.jsx
@@ -4,10 +4,16 @@ function FormLine(props){
   const { inputName, placeholder, limit, toUpperCase, required } = props;
 
   return (
-    <fielset className="form-line">
+    <fieldset className="form-line">
       <label htmlFor={inputName}>{inputName}:</label>
-      <input type="text" id={inputName} placeholder={placeholder} maxLength={limit} className={toUpperCase ? 'upper' : null} required={required ? true : false}/>
-    </fielset>
+      <input 
+        type="text"
+        id={inputName}
+        placeholder={placeholder}
+        maxLength={limit} 
+        className={toUpperCase ? 'upper' : null} 
+        required={required ? true : false}/>
+    </fieldset>
   )
 }
 
