c610dd004fa0f3e3864f4726ba875693be172bdc
defauth98 <neto.daniribeiro@gmail.com>
2021-04-30 15:55:28 -0300

    Filtros por tipo de pokemon

--- frontend/bloco_12/dia_1/src/components/Pokedex.js
+++ frontend/bloco_12/dia_1/src/components/Pokedex.js
@@ -12,6 +12,10 @@ class Pokedex extends React.Component {
       pokemons: data,
       currentIndex: 0,
     };
+
+    this.nextPokemon = this.nextPokemon.bind(this);
+    this.renderFilteredPokemons = this.renderFilteredPokemons.bind(this);
+    this.resetPokemons = this.resetPokemons.bind(this);
   }
 
   renderPokemon() {
@@ -20,12 +24,54 @@ class Pokedex extends React.Component {
     return <Pokemon pokemon={pokemon} />;
   }
 
+  nextPokemon() {
+    if (this.state.currentIndex + 1 === this.state.pokemons.length) {
+      this.setState({
+        currentIndex: 0,
+      });
+
+      return;
+    }
+
+    this.setState({
+      currentIndex: this.state.currentIndex + 1,
+    });
+  }
+
+  renderFilteredPokemons(pokemonType) {
+    const filteredPokemons = data.filter((item) => item.type === pokemonType);
+
+    this.setState({
+      pokemons: filteredPokemons,
+    });
+  }
+
+  resetPokemons() {
+    this.setState({
+      pokemons: data,
+    });
+  }
+
   render() {
     return (
       <div className="pokedex">
         <h1>Pokedex</h1>
 
         <div className="pokemons">{this.renderPokemon()}</div>
+        <div className="buttons-container">
+          <button onClick={this.resetPokemons}>All</button>
+          <button id="fire" onClick={() => this.renderFilteredPokemons('Fire')}>
+            Fire
+          </button>
+          <button
+            id="psychic"
+            onClick={() => this.renderFilteredPokemons('Psychic')}
+          >
+            Psychic
+          </button>
+        </div>
+
+        <button onClick={this.nextPokemon}>Next</button>
       </div>
     );
   }
