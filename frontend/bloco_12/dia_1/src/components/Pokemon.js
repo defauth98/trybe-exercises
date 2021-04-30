ab27104198e14652842bd8b476f44ff41cf1fda1
defauth98 <neto.daniribeiro@gmail.com>
2021-04-30 15:10:50 -0300

    Adicionando prop types do componente Pokemon

--- frontend/bloco_12/dia_1/src/components/Pokemon.js
+++ frontend/bloco_12/dia_1/src/components/Pokemon.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './styles/pokemon.css';
 
-const Pokemon = ({ name, type, value, measurementUnit, image, moreInfo }) => {
-  return (
-    <div className="pokemon">
-      <h2>{name}</h2>
-      <span>{type}</span>
-      <span>
-        {value}
-        {measurementUnit}
-      </span>
-      <img src={image} alt={name} />
-      <a href={moreInfo}>More Info</a>
-    </div>
-  );
+class Pokemon extends React.Component {
+  render() {
+    const { name, type, value, measurementUnit, image, moreInfo } = this.props;
+
+    return (
+      <div className="pokemon">
+        <h2>{name}</h2>
+        <span>{type}</span>
+        <span>
+          {value}
+          {measurementUnit}
+        </span>
+        <img src={image} alt={name} />
+        <a href={moreInfo}>More Info</a>
+      </div>
+    );
+  }
+}
+
+Pokemon.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  measurementUnit: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  moreInfo: PropTypes.string.isRequired,
 };
 
 export default Pokemon;
