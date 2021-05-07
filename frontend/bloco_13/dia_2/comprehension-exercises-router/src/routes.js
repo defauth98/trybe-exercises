f3cc07c3f8898beee2984a458528c2b7cff6c7fb
defauth98 <neto.daniribeiro@gmail.com>
2021-05-07 11:25:01 -0300

    Exercicios

--- frontend/bloco_13/dia_2/comprehension-exercises-router/src/routes.js
+++ frontend/bloco_13/dia_2/comprehension-exercises-router/src/routes.js
@@ -1,42 +1,36 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
-  Link
+  Link,
+  Switch
 } from "react-router-dom";
 
 import About from './components/About'
 import Home from './components/Home'
 import Users from './components/Users'
+import StrictAcess from './components/StrictAcess'
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">home</Link></li>
-            <li><Link to="/about">about</Link></li>
-            <li><Link to="/users">users</Link></li>
-          </ul>
-        </nav>
-        
-      
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-      </div>
-      
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/users/1">Users</Link></li>
+          <li><Link to="/acess">Stric Acess</Link></li>
+          <li><Link to="/denied">Stric Acess Denied</Link></li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/" component={Home} exact/>
+        <Route path="/about" component={About}/>
+        <Route path="/users/:id" render={(props) => <Users {...props} greetingMessage='hi!' />}/> 
+        <Route path="/acess" render={(props) => <StrictAcess {...props} user={{username: 'joao', password:'1234'}} />}/> 
+        <Route path="/denied" render={(props) => <StrictAcess {...props} user={{username: 'daniel', password:'1234'}} />}/> 
+      </Switch>
     </Router>
   );
 }
-
