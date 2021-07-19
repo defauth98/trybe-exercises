43371e4eac6c59f0ffd8e55f9baa13e12319bb88
defauth98 <neto.daniribeiro@gmail.com>
2021-07-19 15:25:42 -0300

    bloco 20 - dia 1

---
+++ backend/bloco_20/dia_1/create-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `sakila`.`FILME` (
+  ` FilmeId` INT NULL AUTO_INCREMENT,
+  `Descricao` VARCHAR(100) NOT NULL,
+  `AnoLancamento` INT NOT NULL,
+  `Nota` INT NULL,
+  PRIMARY KEY (` FilmeId`));
+
