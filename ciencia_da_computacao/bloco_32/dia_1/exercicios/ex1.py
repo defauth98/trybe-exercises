cb5559dc17a1729e75be061f115710afd5153fb5
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2021-11-04 14:55:00 -0300

    exercicios do dia 32.1

---
+++ ciencia_da_computacao/bloco_32/dia_1/exercicios/ex1.py
@@ -0,0 +1,11 @@
+# Exercício 1: Crie uma função que receba dois números e retorne o maior deles.
+
+def bigger(number1, number2):
+    if(number1 > number2):
+        return number1
+    return number2
+
+
+print(bigger(1, 2))
+
+print(bigger(123, 1231))
